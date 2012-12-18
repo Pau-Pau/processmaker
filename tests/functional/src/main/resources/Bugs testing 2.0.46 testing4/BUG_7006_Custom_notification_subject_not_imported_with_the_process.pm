035fc515f056eacd036c8e6be11c05c9b94ddb64
paola.pellegrini@colosa.com <colosa123>
2012-12-18 16:43:46 -0400

    some processes for the testing 4 of version PM 2.0.46

---
+++ tests/functional/src/main/resources/Bugs testing 2.0.46 testing4/BUG_7006_Custom_notification_subject_not_imported_with_the_process.pm
@@ -0,0 +1,48 @@
+000004900O:8:"stdClass":26:{s:7:"process";a:33:{s:7:"PRO_UID";s:32:"44299316450d0d52bb99938011103554";s:10:"PRO_PARENT";s:32:"44299316450d0d52bb99938011103554";s:8:"PRO_TIME";d:1;s:12:"PRO_TIMEUNIT";s:4:"DAYS";s:10:"PRO_STATUS";s:6:"ACTIVE";s:12:"PRO_TYPE_DAY";s:0:"";s:8:"PRO_TYPE";s:6:"NORMAL";s:14:"PRO_ASSIGNMENT";s:5:"FALSE";s:12:"PRO_SHOW_MAP";i:0;s:16:"PRO_SHOW_MESSAGE";i:0;s:14:"PRO_SUBPROCESS";i:0;s:15:"PRO_TRI_DELETED";s:0:"";s:16:"PRO_TRI_CANCELED";s:0:"";s:14:"PRO_TRI_PAUSED";s:0:"";s:18:"PRO_TRI_REASSIGNED";s:0:"";s:17:"PRO_SHOW_DELEGATE";i:0;s:17:"PRO_SHOW_DYNAFORM";i:0;s:12:"PRO_CATEGORY";s:0:"";s:16:"PRO_SUB_CATEGORY";s:0:"";s:12:"PRO_INDUSTRY";i:0;s:15:"PRO_UPDATE_DATE";N;s:15:"PRO_CREATE_DATE";s:19:"2012-12-18 15:42:19";s:15:"PRO_CREATE_USER";s:32:"00000000000000000000000000000001";s:10:"PRO_HEIGHT";i:5000;s:9:"PRO_WIDTH";i:10000;s:11:"PRO_TITLE_X";i:0;s:11:"PRO_TITLE_Y";i:0;s:9:"PRO_DEBUG";i:0;s:13:"PRO_DYNAFORMS";b:0;s:25:"PRO_DERIVATION_SCREEN_TPL";s:0:"";s:15:"PRO_DESCRIPTION";s:0:"";s:9:"PRO_TITLE";s:66:"BUG 7006 Custom notification subject not imported with the process";s:18:"PRO_CATEGORY_LABEL";s:11:"No Category";}s:5:"tasks";a:1:{i:0;a:53:{s:7:"PRO_UID";s:32:"44299316450d0d52bb99938011103554";s:7:"TAS_UID";s:32:"53872680650d0d53ab411d1009171951";s:8:"TAS_TYPE";s:6:"NORMAL";s:12:"TAS_DURATION";d:1;s:14:"TAS_DELAY_TYPE";s:0:"";s:14:"TAS_TEMPORIZER";d:0;s:12:"TAS_TYPE_DAY";s:0:"";s:12:"TAS_TIMEUNIT";s:4:"DAYS";s:9:"TAS_ALERT";s:5:"FALSE";s:21:"TAS_PRIORITY_VARIABLE";s:0:"";s:15:"TAS_ASSIGN_TYPE";s:8:"BALANCED";s:19:"TAS_ASSIGN_VARIABLE";s:30:"@@SYS_NEXT_USER_TO_BE_ASSIGNED";s:18:"TAS_GROUP_VARIABLE";s:0:"";s:24:"TAS_MI_INSTANCE_VARIABLE";s:24:"@@SYS_VAR_TOTAL_INSTANCE";s:24:"TAS_MI_COMPLETE_VARIABLE";s:34:"@@SYS_VAR_TOTAL_INSTANCES_COMPLETE";s:19:"TAS_ASSIGN_LOCATION";s:5:"FALSE";s:25:"TAS_ASSIGN_LOCATION_ADHOC";s:5:"FALSE";s:16:"TAS_TRANSFER_FLY";s:5:"FALSE";s:17:"TAS_LAST_ASSIGNED";s:32:"00000000000000000000000000000001";s:8:"TAS_USER";s:1:"0";s:14:"TAS_CAN_UPLOAD";s:5:"FALSE";s:15:"TAS_VIEW_UPLOAD";s:5:"FALSE";s:33:"TAS_VIEW_ADDITIONAL_DOCUMENTATION";s:5:"FALSE";s:14:"TAS_CAN_CANCEL";s:5:"FALSE";s:13:"TAS_OWNER_APP";s:5:"FALSE";s:7:"STG_UID";s:0:"";s:13:"TAS_CAN_PAUSE";s:5:"FALSE";s:20:"TAS_CAN_SEND_MESSAGE";s:4:"TRUE";s:19:"TAS_CAN_DELETE_DOCS";s:5:"FALSE";s:16:"TAS_SELF_SERVICE";s:5:"FALSE";s:9:"TAS_START";s:4:"TRUE";s:16:"TAS_TO_LAST_USER";s:5:"FALSE";s:19:"TAS_SEND_LAST_EMAIL";s:4:"TRUE";s:14:"TAS_DERIVATION";s:6:"NORMAL";s:8:"TAS_POSX";i:477;s:8:"TAS_POSY";i:119;s:9:"TAS_WIDTH";i:165;s:10:"TAS_HEIGHT";i:40;s:9:"TAS_COLOR";s:0:"";s:11:"TAS_EVN_UID";s:0:"";s:12:"TAS_BOUNDARY";s:0:"";s:25:"TAS_DERIVATION_SCREEN_TPL";s:0:"";s:23:"TAS_SELFSERVICE_TIMEOUT";i:0;s:20:"TAS_SELFSERVICE_TIME";s:0:"";s:25:"TAS_SELFSERVICE_TIME_UNIT";s:0:"";s:27:"TAS_SELFSERVICE_TRIGGER_UID";s:0:"";s:9:"TAS_TITLE";s:6:"Task 1";s:15:"TAS_DESCRIPTION";s:0:"";s:13:"TAS_DEF_TITLE";s:0:"";s:19:"TAS_DEF_DESCRIPTION";s:0:"";s:17:"TAS_DEF_PROC_CODE";s:0:"";s:15:"TAS_DEF_MESSAGE";s:18:"holaaaaaaaaaaaaaaa";s:23:"TAS_DEF_SUBJECT_MESSAGE";s:23:"prueba de notifications";}}s:6:"routes";a:1:{i:0;a:17:{s:7:"ROU_UID";s:32:"47000122050d0d541a69ca8081313325";s:10:"ROU_PARENT";s:1:"0";s:7:"PRO_UID";s:32:"44299316450d0d52bb99938011103554";s:7:"TAS_UID";s:32:"53872680650d0d53ab411d1009171951";s:13:"ROU_NEXT_TASK";s:2:"-1";s:8:"ROU_CASE";s:1:"1";s:8:"ROU_TYPE";s:10:"SEQUENTIAL";s:13:"ROU_CONDITION";s:0:"";s:16:"ROU_TO_LAST_USER";s:5:"FALSE";s:12:"ROU_OPTIONAL";s:5:"FALSE";s:14:"ROU_SEND_EMAIL";s:4:"TRUE";s:16:"ROU_SOURCEANCHOR";s:1:"1";s:16:"ROU_TARGETANCHOR";s:1:"0";s:11:"ROU_TO_PORT";s:1:"1";s:13:"ROU_FROM_PORT";s:1:"2";s:11:"ROU_EVN_UID";s:0:"";s:7:"GAT_UID";s:0:"";}}s:5:"lanes";a:0:{}s:8:"gateways";a:0:{}s:6:"inputs";a:0:{}s:7:"outputs";a:0:{}s:9:"dynaforms";a:1:{i:0;a:6:{s:7:"DYN_UID";s:32:"80047607450d0ea05b6b357017444615";s:7:"PRO_UID";s:32:"44299316450d0d52bb99938011103554";s:8:"DYN_TYPE";s:7:"xmlform";s:12:"DYN_FILENAME";s:65:"44299316450d0d52bb99938011103554/80047607450d0ea05b6b357017444615";s:9:"DYN_TITLE";s:4:"form";s:15:"DYN_DESCRIPTION";s:0:"";}}s:5:"steps";a:1:{i:0;a:8:{s:8:"STEP_UID";s:32:"47488403350d0eba6d31599015685036";s:7:"PRO_UID";s:32:"44299316450d0d52bb99938011103554";s:7:"TAS_UID";s:32:"53872680650d0d53ab411d1009171951";s:13:"STEP_TYPE_OBJ";s:8:"DYNAFORM";s:12:"STEP_UID_OBJ";s:32:"80047607450d0ea05b6b357017444615";s:14:"STEP_CONDITION";s:0:"";s:13:"STEP_POSITION";i:1;s:9:"STEP_MODE";s:4:"EDIT";}}s:8:"triggers";a:0:{}s:9:"taskusers";a:0:{}s:8:"groupwfs";a:0:{}s:12:"steptriggers";a:0:{}s:13:"dbconnections";a:0:{}s:12:"reportTables";a:0:{}s:16:"reportTablesVars";a:0:{}s:14:"stepSupervisor";a:0:{}s:17:"objectPermissions";a:0:{}s:10:"subProcess";a:0:{}s:11:"caseTracker";a:0:{}s:17:"caseTrackerObject";a:0:{}s:5:"stage";a:0:{}s:14:"fieldCondition";a:0:{}s:5:"event";a:0:{}s:13:"caseScheduler";a:0:{}s:15:"processCategory";N;}00000003280047607450d0ea05b6b357017444615000000380<?xml version="1.0" encoding="UTF-8"?>
+<dynaForm type="xmlform" name="44299316450d0d52bb99938011103554/80047607450d0ea05b6b357017444615" width="500" enabletemplate="0" mode="" nextstepsave="prompt">
+  <text type="text" maxlength="64" validate="Any" size="15" mode="edit">
+    <en>Escriba algo</en>
+  </text>
+  <aceptar type="submit">
+    <en>Aceptar</en>
+  </aceptar>
+</dynaForm>
+MAILTEMPL000000018alert_message.html000000682
+<table style=\"background-color: white; font-family: Arial,Helvetica,sans-serif; color: black; font-size: 11px; text-align: left;\" cellpadding='10' cellspacing='0' width='100%'>
+<tbody>	
+		<tr><td style='font-size: 14px;'><b>ALERT MESSAGE</b></td></tr>
+		<tr><td style='vertical-align:middel;'>
+				<hr>
+				<br />
+						The Case <b>@#APP_NUMBER</b>, currently in the task <b>@#TAS_TITLE</b>, expired on <b>@#DEL_TASK_DUE_DATE</b>.						
+				<br />		
+				<br />
+				<hr><b>This Business Process is powered by ProcessMaker.<b><br />
+						<a href='http://www.processmaker.com' style='color:#c40000;'>www.processmaker.com</a>
+				<br />
+		</td></tr>
+</tbody>
+</table>000000022unassignedMessage.html000000829<table width="100%" cellpadding="10" cellspacing="0" style="background: #FFFFFF; font: 11px arial, verdana, helvetica, sans-serif;">
+    <tr>
+        <td style="font-size: 14px;">
+            <strong>UNASSIGNED CASE</strong>
+            <hr />
+        </td>
+    </tr>
+    <tr>
+        <td>
+            The case <strong>#@#APP_NUMBER</strong> is available on the unassigned list.
+            <br />
+            <br />
+            Note: If you want to claim the case and it is not available on the list, it might be already taken.
+        </td>
+    </tr>
+    <tr>
+        <td>
+            <hr />
+            This Business Process is powered by ProcessMaker.
+            <br />
+            <a href="http://www.processmaker.com" style="color: #C40000;">www.processmaker.com</a>
+        </td>
+    </tr>
+</table>
